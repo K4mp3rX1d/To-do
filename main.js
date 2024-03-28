397c4a9580b3cedc36227105c992b7deb3ef4f54
K4mp3rX1d <141189533+K4mp3rX1d@users.noreply.github.com>
2024-03-28 20:37:32 +0600

    JS code moved to main.js
    Codeswing extention was not initiating if script.js is not blank.

---
+++ main.js
@@ -0,0 +1,42 @@
+const input = document.querySelector('input');
+// input.focus();
+const button = document.querySelector('.button')
+const list = document.querySelector('ul');
+
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        addToList();
+    }
+});
+
+
+function addToList() {
+    const inputText = input.value;
+    input.value = "";
+
+    const done = document.createElement('div');
+    done.innerHTML = "&check;"
+    done.classList.toggle('status');
+    done.addEventListener('click', () => {
+        // IMPROVE THIS...
+        done.parentElement.classList.add('.done');
+    });
+
+    const item = document.createElement('li');
+    const itemText = document.createElement('span');
+    itemText.textContent = inputText;
+
+    const itemRemove = document.createElement('div');
+    itemRemove.innerHTML = '&#10060;';
+    itemRemove.classList.toggle('item-remove');
+    itemRemove.addEventListener('click', () => {
+        itemRemove.parentNode.remove();
+    });
+
+    item.appendChild(done);
+    item.appendChild(itemText);
+    item.appendChild(itemRemove);
+    list.appendChild(item);
+    input.focus();
+}
